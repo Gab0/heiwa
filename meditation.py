a97d7a9bd47ce69f8565b134d11fedf7689e9e1e
Gab0 <gabriel_scf@hotmail.com>
2017-07-26 19:27:28 -0300

    .

--- meditation.py
+++ meditation.py
@@ -1,14 +1,21 @@
 #!/bin/python2
-#!-*- encoding: utf8 -*-
+#!-*- encoding: utf-8 -*-
 
 from playsound import playsound
 from time import sleep
 import optparse
 import sys
+from os import path, chdir
+#from urllib.request import urlopen
+import random 
+
 parser = optparse.OptionParser()
+chdir(path.dirname(path.realpath(__file__)))
 
 parser.add_option("--timer", dest = "GongoTimer", default = 3, type = 'int', action = 'store', help = 'Define the interval between the gongos {minutes}.')
 parser.add_option("--count", dest = "GongoCount", default = 8, type = 'int', action = 'store', help = 'Define the total number of gongos.')
+parser.add_option("--caos", dest = "Random", default = False, action = "store_true")
+
 # TODO: ADD RANDOM TIMER;
 (options, args) = parser.parse_args()
 
@@ -34,7 +41,15 @@ print(HEIWA)
 for GONGO in range(options.GongoCount):
     playsound(gong)
 
-    for TIMER in range(60):
+    time = 60
+    #q=urlopen('http://random-ize.com/random-youtube/').read()
+
+    if options.Random:
+        time = time*random.randrange(5e7 , 15e7)/1e8
+        time = int(round(time))
+        print(time)
+
+    for TIMER in range(time):
         sys.stdout.write("#")
         sys.stdout.flush()
         sleep(options.GongoTimer)
